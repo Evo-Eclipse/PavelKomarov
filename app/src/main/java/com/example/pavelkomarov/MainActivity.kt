294be218d5685ef1964f365785b7fd7d3cedfbae
Evo Eclipse <evo.galaio@outlook.com>
2025-03-21 23:05:19 +0300

    Cleanup and Upgrade AGP

--- app/src/main/java/com/example/pavelkomarov/MainActivity.kt
+++ app/src/main/java/com/example/pavelkomarov/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
@@ -15,13 +17,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        // Could be replaced with: activity_main_relative, ...
-        setContentView(R.layout.activity_main_linear)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets // Equivalent to `return insets`
-//        }
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets // Equivalent to `return insets`
+        }
         Log.d(TAG, "onCreate вызван")
 
         // Программный переход по нажатию кнопки
